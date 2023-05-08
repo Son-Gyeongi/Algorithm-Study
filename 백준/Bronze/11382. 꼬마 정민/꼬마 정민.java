5847213cbb1fdb4e8a34ca9b7038e426f9a7e9f1
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2023-05-08 23:51:05 +0900

    [Bronze V] Title: 꼬마 정민, Time: 112 ms, Memory: 12892 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        Long a = s.nextLong();
+        Long b = s.nextLong();
+        Long c = s.nextLong();
+
+        System.out.println(a+b+c);
+    }
+}
