037ddcd4163cbee3377bef12f10822edecd5d877
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2023-10-19 09:20:24 +0900

    [level unrated] Title: 짝수의 합, Time: 0.02 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+       
+        for (int i=n;0<=i;i--) {
+            if(i%2==0) answer += i;
+        }
+        
+        return answer;
+    }
+}
