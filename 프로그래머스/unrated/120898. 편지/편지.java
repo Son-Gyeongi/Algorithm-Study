807500dd7a8e9a854c9bebebc53408faf37cc5c8
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2023-10-23 02:17:01 +0900

    [level unrated] Title: 편지, Time: 0.01 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120898. 편지/편지.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String message) {
+        /*
+        "happy birthday!"
+        공백도 하나의 문자
+        문자 하나당 가로 2cm
+        */
+        
+        int length = message.length();
+        
+        return length *2;
+    }
+}
