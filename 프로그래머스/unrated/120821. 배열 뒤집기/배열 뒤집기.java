b5f1056dfb2cae71ac2b8f613a8c901beb553db6
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2023-10-24 02:25:50 +0900

    [level unrated] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,13 +1,10 @@
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length];
-        int tmp = 0;
         int arrLen = num_list.length -1;
         
         for (int i=0; i<=arrLen ; i++) {
-            tmp = num_list[i];
             answer[i] = num_list[arrLen - i];
-            answer[arrLen - i] = tmp;
         }
         
         return answer;
