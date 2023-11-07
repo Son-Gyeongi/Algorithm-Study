78d749c56f6ba4b609768c0a80b86ab637803124
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2023-11-07 10:58:19 +0900

    [level unrated] Title: 순서쌍의 개수, Time: 1105.76 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int cnt = 0;
+
+        // 제곱근 구하기
+        int result = (int) Math.sqrt(n);
+
+        for (int i = 0; i <= result; i++) {
+            for (int j = n; j > 0; j--) {
+                if (i * j == n) {
+                    cnt++;
+                }
+            }
+        }
+        answer = answer+(cnt*2);
+
+        if (Math.pow(result, 2) == n) {
+            answer--;
+        }
+
+        return answer;
+    }
+}
