13e0a39ebd2d76044131a64feae1bc8e1062fcbb
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2023-12-16 00:25:17 +0900

    [level unrated] Title: 꼬리 문자열, Time: 1.99 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        return Arrays.stream(str_list)
+                .filter(str -> !str.contains(ex))
+                .reduce("", (acc, str) -> acc + str);
+    }
+}
