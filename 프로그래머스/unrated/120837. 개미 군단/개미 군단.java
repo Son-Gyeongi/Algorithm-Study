32c520d232a3163b4184efd7add801c219d7e3f4
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2023-12-31 05:48:20 +0900

    [level unrated] Title: 개미 군단, Time: 0.01 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+
+        // 장군개미는 5의 공격력을, 병정개미는 3의 공격력을 일개미는 1의 공격력
+        if (hp / 5 > 0) {
+            answer = hp / 5;
+            hp %= 5;
+        }
+        if (hp / 3 > 0) {
+            answer += (hp / 3);
+            hp %= 3;
+        }
+        answer += hp;
+
+        return answer;
+    }
+}
