30269f7a65c46cbafc216f1bc8d2914217b1e38e
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-01-01 07:25:16 +0900

    [level unrated] Title: 주사위의 개수, Time: 1.25 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120845. 주사위의 개수/주사위의 개수.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] box, int n) {
+        return Arrays.stream(box).map(i -> i / n)
+                .reduce(1, (i, acc) -> i * acc);
+    }
+}
