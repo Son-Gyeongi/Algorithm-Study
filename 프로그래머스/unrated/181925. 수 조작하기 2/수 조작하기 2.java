c7c011384c74b6e1f797994962f3accd7cce2248
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-01-09 17:24:42 +0900

    [level unrated] Title: 수 조작하기 2, Time: 8.40 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < numLog.length - 1; i++) {
+            int num = numLog[i + 1] - numLog[i];
+
+            switch (num) {
+                case 1:
+                    answer.append("w");
+                    break;
+                case -1:
+                    answer.append("s");
+                    break;
+                case 10:
+                    answer.append("d");
+                    break;
+                case -10:
+                    answer.append("a");
+                    break;
+            }
+        }
+
+        return answer.toString();
+    }
+}
