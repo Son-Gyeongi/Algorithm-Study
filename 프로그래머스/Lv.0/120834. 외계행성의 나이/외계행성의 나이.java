d0d467591b06e21e62eebb101c96159f62fcce43
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-01-13 07:16:39 +0900

    [level Lv.0] Title: 외계행성의 나이, Time: 5.45 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(int age) {
+        StringBuilder answer = new StringBuilder();
+
+        // a는 0, b는 1, c는 2, ..., j는 9
+        String str = age + "";
+
+        for (int i = 0; i < str.length(); i++) {
+            switch (str.charAt(i)) {
+                case '0' : answer.append("a"); break;
+                case '1' : answer.append("b"); break;
+                case '2' : answer.append("c"); break;
+                case '3' : answer.append("d"); break;
+                case '4' : answer.append("e"); break;
+                case '5' : answer.append("f"); break;
+                case '6' : answer.append("g"); break;
+                case '7' : answer.append("h"); break;
+                case '8' : answer.append("i"); break;
+                case '9' : answer.append("j"); break;
+            }
+        }
+
+        return answer.toString();
+    }
+}
