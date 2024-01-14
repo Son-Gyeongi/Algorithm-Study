7b1641206cdd6eb4dee76625e9b8754b818a5069
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-01-14 10:29:46 +0900

    [level Lv.0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        boolean option1 = a == b;
+        boolean option2 = b == c;
+        boolean option3 = c == a;
+
+        if (option1 && option2) { // opiton3은 항상 true
+            return (a + b + c) * ((a * a) + (b * b) + (c * c)) * ((a * a * a) + (b * b * b) + (c * c * c));
+        } else if (option1 || option2 || option3) {
+            return (a + b + c) * ((a * a) + (b * b) + (c * c));
+        } else return a + b + c;
+    }
+}
