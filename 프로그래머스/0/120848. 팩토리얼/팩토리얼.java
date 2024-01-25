8dcee4dd8d7af64901f47c5243f492100507d667
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-01-25 10:58:56 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+
+        if (n == 3628800) return 10;
+        else if (362880 <= n && n < 3628800) return 9;
+        else if (40320 <= n && n < 362880) return 8;
+        else if (5040 <= n && n < 40320) return 7;
+        else if (720 <= n && n < 5040) return 6;
+        else if (120 <= n && n < 720) return 5;
+        else if (24 <= n && n < 120) return 4;
+        else if (6 <= n && n < 24) return 3;
+        else if (2 <= n && n < 6) return 2;
+
+        return answer;
+    }
+}
