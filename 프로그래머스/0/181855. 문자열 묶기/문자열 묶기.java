2eb862199b8bc1c69680e84ce850db810275fd22
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-04-20 17:48:00 +0900

    [level 0] Title: 문자열 묶기, Time: 14.47 ms, Memory: 120 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(String[] strArr) {
+        int[] strCount = new int[strArr.length];
+
+        for (int i = 0; i < strCount.length; i++) {
+            strCount[i] = strArr[i].length();
+        }
+
+        Map<Integer, Integer> myMap =new HashMap<>();
+
+        Arrays.stream(strCount).forEach(str -> {
+            // 값이 있으면
+            if (myMap.containsKey(str)) {
+                Integer count = myMap.get(str);
+                myMap.put(str, ++count);
+            } else { // 값이 없으면
+                myMap.put(str, 1);
+            }
+        });
+
+        // Map 반복문으로 순회, 큰 값 꺼내기
+        int max = 0;
+        for (Map.Entry<Integer, Integer> entry : myMap.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+
+            if (max < value) max = value;
+        }
+
+        return max;
+    }
+}
