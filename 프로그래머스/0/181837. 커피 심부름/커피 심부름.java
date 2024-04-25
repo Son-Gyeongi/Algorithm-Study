a188100d9cf36d67907913728d60038afe328a3e
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-04-25 11:26:15 +0900

    [level 0] Title: 커피 심부름, Time: 0.98 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String[] order) {
+        // 아메리카노 4,500원 / 카페라테 5,000원
+        // 차가운 지 따뜻한 지 구별 안해도 됨
+        // "anything"인 '아무거나'는 아메리카노
+
+        int americanoCount = (int) Arrays.stream(order)
+                .filter(o1 -> o1.contains("americano") || o1.contains("anything"))
+                .count();
+
+        int cafelatteCount = (int) Arrays.stream(order)
+                .filter(o2 -> o2.contains("cafelatte"))
+                .count();
+
+        return (americanoCount * 4500) + (cafelatteCount * 5000);
+    }
+}
