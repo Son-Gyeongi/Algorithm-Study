5b2cd08b0252ca5d7a5662fffcf631279612f59e
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-05-27 18:40:19 +0900

    [level 0] Title: 저주의 숫자 3, Time: 7.11 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i=1;i<=n;i++) {
+
+            answer++;
+
+            while (answer %3 == 0 && answer > 0) answer++;
+
+            // 문자열로 3 포함하는지 보기
+            String str = String.valueOf(answer);
+            while(str.contains("3")) {
+                answer++;
+                str = String.valueOf(answer);
+            }
+
+            while (answer %3 == 0 && answer > 0) answer++;
+            System.out.printf("%d ==> %d\n", i, answer);
+        }
+
+        return answer;
+    }
+}
