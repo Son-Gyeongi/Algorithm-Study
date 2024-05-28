c2c8b52d852ec82545edfcbe71d9d9880476317c
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-05-28 11:27:37 +0900

    [level 0] Title: 다항식 더하기, Time: 7.73 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String polynomial) {
+        String answer = "";
+
+        String[] strPol = polynomial.split(" \\+ "); // 더하기 이스케이프하기
+
+        int linearTerm = 0;
+        int constant = 0;
+        for (int i=0;i<strPol.length;i++) {
+            if (strPol[i].contains("x")) {
+                String[] xSpl = strPol[i].split("x");
+                if (xSpl.length == 0) linearTerm += 1;
+                else linearTerm += Integer.parseInt(xSpl[0]);
+            } else {
+                constant += Integer.parseInt(strPol[i]);
+            }
+        }
+
+        if (linearTerm == 0) answer += constant;
+        else if (linearTerm == 1) answer = constant == 0 ? "x" : "x + " +constant;
+        else if (constant == 0) answer = linearTerm + "x";
+        else answer = linearTerm + "x + " + constant;
+
+        return answer;
+    }
+}
