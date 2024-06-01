e68b71c4b565f08c8a2bd4e14fb42bbe6c92155b
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-06-01 18:28:41 +0900

    [level 0] Title: 최빈값 구하기, Time: 1.78 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+
+        int[] numArr = new int[1000];
+
+        // numArr 배열에 array 배열 값을 인덱스에 맞춰서 넣기
+        for (int i=0;i<numArr.length;i++) {
+            for (int j=0;j<array.length;j++) {
+                if (i == array[j]) {
+                    numArr[i] += 1;
+                }
+            }
+        }
+
+        // numArr에서 Max 값 찾고 해당 인덱스 알아내기
+        int max = Arrays.stream(numArr).max().getAsInt();
+
+        int count = 0;
+        for (int i = 0; i < numArr.length; i++) {
+            if (numArr[i] == max) {
+                answer = i;
+                count++;
+            }
+        }
+
+        if (count > 1) answer = -1;
+
+        return answer;
+    }
+}
