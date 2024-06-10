18cbfed0cca7a4db3a48ccafd19f7ba6aec11934
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-06-11 06:22:24 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 2.28 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] str = s.split("");
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String string : str) {
+            // 값이 있으면
+            if (map.containsKey(string)) {
+                Integer count = map.get(string);
+                map.put(string, ++count);
+            } else {
+                // 값이 없으면
+                map.put(string, 1);
+            }
+        }
+
+        for (String string : str) {
+            Integer count = map.get(string);
+
+            if (count == 1) {
+                answer += string;
+            }
+        }
+
+        // answer 영어 오름차순으로 정렬
+        String[] strSplit = answer.split("");
+        Arrays.sort(strSplit);
+
+        return String.join("", strSplit);
+    }
+}
