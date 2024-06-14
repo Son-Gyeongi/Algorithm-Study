a82f6261e1a17bce6b8519631ee4a1c510c55ac7
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-06-14 22:34:10 +0900

    [level 0] Title: 가까운 수, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.Math;
+
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        int[] arr = new int[array.length];
+        
+        // array 배열의 각 요소에서 n 값을 뺀 것을 arr 배열에 저장하기
+        int min = Math.abs(array[0] - n);
+        for (int i=0;i<array.length;i++) {
+            arr[i] = Math.abs(array[i] - n);
+            
+            min = Math.min(min, arr[i]);
+        }
+        
+        int temp = 0;
+        for (int i=0;i<array.length;i++) {
+            if(min == arr[i]) {
+                if (temp == 0)
+                    temp = array[i];
+                answer = Math.min(temp, array[i]);
+            }
+        }
+        
+        return answer;
+    }
+}
