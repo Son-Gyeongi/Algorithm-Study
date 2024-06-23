5bdd8e177943c75668ef65cfc0b8319aa7172291
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-06-23 20:57:29 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int[][] dots) {
+        int answer = 0;
+        
+        // 0,1,2,3 은 dots의 인덱스를 말함
+        // 좌표 0,1 와 2,3 일 경우
+        if ((dots[1][0] - dots[0][0] == dots[3][0] - dots[2][0])
+            && (dots[1][1] - dots[0][1] == dots[3][1] - dots[2][1])) return 1;
+        else if (((double)(dots[1][1] - dots[0][1])/(dots[1][0] - dots[0][0]))
+                    == ((double)(dots[3][1] - dots[2][1]) / (dots[3][0] - dots[2][0]))) return 1;
+        
+        // 좌표 0,2 와 1,3 일 경우
+        if ((dots[2][0] - dots[0][0] == dots[3][0] - dots[1][0])
+            && (dots[2][1] - dots[0][1] == dots[3][1] - dots[1][1])) return 1;
+        else if (((double)(dots[2][1] - dots[0][1])/(dots[2][0] - dots[0][0]))
+                    == ((double)(dots[3][1] - dots[1][1]) / (dots[3][0] - dots[1][0]))) return 1;
+        
+        // 좌표 0,3 와 1,2 일 경우
+        if ((dots[3][0] - dots[0][0] == dots[2][0] - dots[1][0])
+            && (dots[3][1] - dots[0][1] == dots[2][1] - dots[1][1])) return 1;
+        else if (((double)(dots[3][1] - dots[0][1])/(dots[3][0] - dots[0][0]))
+                    == ((double)(dots[2][1] - dots[1][1]) / (dots[2][0] - dots[1][0]))) return 1;
+
+        return answer;
+    }
+}
