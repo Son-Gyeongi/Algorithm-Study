4bcbd6132f862c0c127d3443266c37401aaccd56
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-06-25 00:58:16 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.06 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -0,0 +1,76 @@
+// import java.util.*;
+
+class Solution {
+    public int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+        int column = n; // 열 → 오른쪽 화살표 계산
+        int row = n; // 행 ↓ 아래쪽 화살표 계산
+        int firstColumn = 0; // 열 ← 왼쪽 화살표 계산
+        int firstRow = 0; // 행 ↑ 위쪽 화살표 계산
+        int count = 1;
+        int x = 0; // answer[x][y]
+        int y = 0;
+
+        // right, left 결정
+        boolean rlSwitch = true; // true일 경우 오른쪽으로 false일 경우 왼쪽으로
+        // up, down 결정
+        boolean udSwitch = true; // true일 경우 아래쪽으로 false일 경우 위쪽으로
+
+
+        while(true) {
+            if (rlSwitch) {
+                // →
+                for (int i=y;i<column;i++) {
+                    answer[x][i] = count;
+                    count++;
+                    y = i;
+                }
+                x += 1;
+                rlSwitch = false;
+                column--;
+            }
+
+            if (udSwitch) {
+                // ↓
+                for (int i=x;i<row;i++) {
+                    answer[i][y] = count;
+                    count++;
+                    x = i;
+                }
+                y -= 1;
+                udSwitch = false;
+                row--;
+            }
+
+            if (!rlSwitch) {
+                // ←
+                for (int i=y;i>=firstColumn;i--) {
+                    answer[x][i] = count;
+                    count++;
+                    y = i;
+                }
+                x -= 1;
+                rlSwitch = true;
+                firstColumn++;
+            }
+
+            if (!udSwitch) {
+                // ↑
+                for (int i=x;i>firstRow;i--) {
+                    answer[i][y] = count;
+                    count++;
+                    x = i;
+                }
+                y += 1;
+                udSwitch = true;
+                firstRow++;
+            }
+
+            if (count == n * n + 1) break;
+        }
+
+        // System.out.println(Arrays.deepToString(answer));
+
+        return answer;
+    }
+}
