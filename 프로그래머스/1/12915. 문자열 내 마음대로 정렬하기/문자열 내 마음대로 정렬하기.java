403294134650af35a0922981653cc4131b4b902f
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-07-02 23:54:48 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.41 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        String[] answer = new String[strings.length];
+        int index = 0;
+
+        // 1. a-z까지 반복문 돌리기
+        for (char start='a';start<='z';start++) {
+            int count = 0;
+            // 2. strings 배열에서 각 요소들 중에 n인덱스랑 맞는 a-z 찾고 stringList에 넣기
+            List<String> tempList = new ArrayList<>();
+            for (String string : strings) {
+                if (start == string.charAt(n)) { // 3. 같은 값일 경우 비교 후에 넣기
+                    tempList.add(string);
+                    count++;
+                }
+            }
+
+            // tempList 정렬
+//            List<String> sortedList = tempList.stream().sorted().toList(); // 자바 16에서 추가된 것, 오름차순
+//            Collections.sort(tempList); // 오름차순
+            tempList.sort(Comparator.naturalOrder()); // 자바 8에서 추가된 것, 오름차순
+            // amswer 에 값 넣기
+            for (int i=0;i<tempList.size();i++) {
+                answer[index++] = tempList.get(i);
+            }
+        }
+
+        return answer;
+    }
+}
