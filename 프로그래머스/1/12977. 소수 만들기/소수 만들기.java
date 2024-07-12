d1992655f750ce7795c479e1c2daf3cd81496630
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-07-12 22:14:50 +0900

    [level 1] Title: 소수 만들기, Time: 15.96 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        // sum 저장
+        List<Integer> list = new ArrayList<>();
+
+        for (int i=0;i<nums.length-2;i++) {
+            int a = nums[i];
+            
+            for (int j=i+1;j<nums.length-1;j++) {
+                int b = nums[j];
+                
+                for (int k=j+1;k<nums.length;k++) {
+                    int c = nums[k];
+                    int sum = a + b + c;
+                
+                    list.add(sum);
+                }
+            }
+        }
+        
+        for (int i=0;i<list.size();i++) {
+            if (primeNumber(list.get(i))) answer++;
+        }
+
+        return answer;
+    }
+    
+    public boolean primeNumber(int primeNumber) {
+        // primeNumber가 소수면 true, 아니면 false
+        for (int i=2;i<primeNumber;i++) {
+            if (primeNumber % i == 0) return false;
+        }
+        
+        return true;
+    }
+}
