df3533b75866b9df1701b99cbae87b32d6f274da
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-07-23 23:29:31 +0900

    [level 1] Title: 대충 만든 자판, Time: 16.05 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {
+        int[] answer = new int[targets.length];
+        Map<String, Integer> map = new HashMap<>();
+        
+        // 1. Map 에 keymap 각 요소 누르는 값 넣기
+        for (int i=0;i<keymap.length;i++) {
+            String[] key = keymap[i].split("");
+            
+            for (int j=0;j<key.length;j++) {
+                int value = map.getOrDefault(key[j], j+1);
+                
+                map.put(key[j], value > j+1 ? j+1 : value);
+            }
+        }
+        
+        // 2. map에 맞게 targets 계산하기
+        for (int i=0;i<targets.length;i++) {
+            String[] target = targets[i].split("");
+            
+            for (int j=0;j<target.length;j++) {
+                if (map.get(target[j]) == null) {
+                    answer[i] = -1;
+                    break;
+                }
+                
+                int value = map.get(target[j]);
+                
+                answer[i] += value;
+            }
+        }
+        
+        return answer;
+    }
+}
