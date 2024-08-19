00563eb3a245cfab9e87797d4752bc6f58f98054
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-08-19 21:49:07 +0900

    [level 2] Title: 최솟값 만들기, Time: 7.24 ms, Memory: 52.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+
+        // 1. A 배열을 내림차순으로 저장하기
+        Integer[] a = new Integer[A.length];
+        
+        for (int i=0;i<A.length;i++) {
+            a[i] = A[i];
+        }
+        Arrays.sort(a, Collections.reverseOrder());
+        
+        // 1-1. B 배열을 오름차순으로 저장하기
+        Integer[] b = new Integer[B.length];
+        
+        for (int i=0;i<B.length;i++) {
+            b[i] = B[i];
+        }
+        Arrays.sort(b);
+        
+        // 2. A 각 요소와 B 모든 요소를 곱해서 작은 값을 answer에 더하기
+        for (int i=0;i<a.length;i++) {
+            int mulA = a[i];
+            int mulB = b[i];
+            
+            answer += (mulA * mulB);
+        }
+
+        return answer;
+    }
+}
