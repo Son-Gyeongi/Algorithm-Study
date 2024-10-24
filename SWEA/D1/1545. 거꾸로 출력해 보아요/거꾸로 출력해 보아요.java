a36b84221e0dc6d4ccdaa153f59a4cab6cbabfbf
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-10-24 18:06:01 +0900

    [D1] Title: 거꾸로 출력해 보아요, Time: 134 ms, Memory: 20,208 KB -BaekjoonHub

---
+++ SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int N;
+		N=sc.nextInt();
+
+        for (int i=N;i>=0;i--) {
+            System.out.printf("%d ",i);
+        }
+	}
+}
