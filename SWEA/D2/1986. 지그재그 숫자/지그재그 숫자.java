4daf8f484df11f7c94662374044279d12a21843e
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-10-26 17:58:46 +0900

    [D2] Title: 지그재그 숫자, Time: 131 ms, Memory: 20,280 KB -BaekjoonHub

---
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            System.out.printf("#%d ", test_case);
+            int num = sc.nextInt();
+            int sum = 0;
+            
+            for (int i=1;i<=num;i++) {
+                if (i%2 == 1) sum += i;
+                else sum -= i;
+            }
+            
+            System.out.println(sum);
+		}
+	}
+}
