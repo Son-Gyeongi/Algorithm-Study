63e9d3adb06cdc23c4a47e938b6c3fc1de48dd06
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-11-05 17:34:37 +0900

    [D2] Title: +=, Time: 202 ms, Memory: 29,936 KB -BaekjoonHub

---
+++ SWEA/D2/21425. ＋＝/＋＝.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for (int tc=0;tc<T;tc++) {
+            // x,y 숫자가 주어질 때 숫자가 작은 쪽이 기준이 된다.
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int N = sc.nextInt();
+            
+            int count = 0;
+            while (x <= N && y <= N) {
+                if (x <= y) {
+                    x = x + y;
+                    count++;
+                } else {
+                    y = y + x;
+                    count++;
+                }
+            }
+            
+            System.out.println(count);
+        }
+	}
+}
