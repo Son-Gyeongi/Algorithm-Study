d862842de4eba7323a9f028bd6aa092d29eb8a47
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-11-22 11:14:06 +0900

    [D3] Title: [Professional] 쥬스 나누기, Time: 175 ms, Memory: 23,972 KB -BaekjoonHub

---
+++ SWEA/D3/5601. ［Professional］ 쥬스 나누기/［Professional］ 쥬스 나누기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            System.out.printf("#%d ",test_case);
+            int N = sc.nextInt();
+            
+            for (int i=0;i<N;i++) {
+                System.out.print("1/"+N+" ");
+            }
+            
+            System.out.println();
+		}
+	}
+}
