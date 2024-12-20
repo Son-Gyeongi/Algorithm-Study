9f03c1ddb4206ed0a85eddd580093dc1c0c6fe07
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2024-12-20 18:14:51 +0900

    [D3] Title: 두가지 빵의 딜레마, Time: 222 ms, Memory: 29,036 KB -BaekjoonHub

---
+++ SWEA/D3/5162. 두가지 빵의 딜레마/두가지 빵의 딜레마.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            System.out.printf("#%d ",test_case);
+            int min = Math.min(sc.nextInt(), sc.nextInt());
+            System.out.println(sc.nextInt()/min);
+		}
+	}
+}
