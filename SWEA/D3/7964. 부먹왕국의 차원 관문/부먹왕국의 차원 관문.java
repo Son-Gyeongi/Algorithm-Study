ec1f22f093107b00b74a4d46b8ba54f0013ca3f5
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2025-01-14 14:36:54 +0900

    [D3] Title: 부먹왕국의 차원 관문, Time: 780 ms, Memory: 126,520 KB -BaekjoonHub

---
+++ SWEA/D3/7964. 부먹왕국의 차원 관문/부먹왕국의 차원 관문.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            int answer = 0;
+            int n = sc.nextInt();
+            int d = sc.nextInt();
+            int count = 0; // 1과 1 사이의 0 개수 구하기
+            
+            if (d == 1) {
+                for (int i=0;i<n;i++) {
+                    if (sc.nextInt() == 0) answer++;
+                }
+                
+                System.out.println("#"+test_case+" "+answer);
+                continue;
+            }
+            
+            for (int i=0;i<n;i++) {
+                if (sc.nextInt() == 0) count++;
+                else {
+                    if (count > 0) {
+                        answer += count/d;
+                        count = 0; // 다시 0 개수 세기
+                    }
+                }
+            }
+            
+            System.out.println("#"+test_case+" "+answer);
+		}
+	}
+}
