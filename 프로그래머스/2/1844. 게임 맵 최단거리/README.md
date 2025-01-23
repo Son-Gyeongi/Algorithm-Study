dcc7c4b8e6013a4cee9ccbc2e09a2742bfe723a4
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2025-01-23 20:59:23 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.66 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844#) 
 
 ### 성능 요약
 
-메모리: 56.1 MB, 시간: 12.75 ms
+메모리: 55.6 MB, 시간: 8.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 23일 19:28:13
+2025년 01월 23일 20:59:21
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,72 +1,40 @@
 import java.util.*;
 
 class Solution {
-    public static int n;
-    public static int m;
-    
     public int solution(int[][] maps) {
-        int answer = -1;
-        
-        n = maps.length; // 행 길이
-        m = maps[0].length; // 열 길이
-        
-        if (n == 1) {
-            if (maps[0][m-2] != 0) answer = m;
-        } else if (m == 1) {
-            if (maps[n-2][0] != 0) answer = n;
-        } else if (maps[n-2][m-1] == 0 && maps[n-1][m-2] == 0) {
-            // 상대 팀 진영이 막혀있을 경우
-            answer = -1;
-        } else {            
-            answer = directions(maps);
-        }
-        
-        return answer;
-    }
-    
-    public static int directions(int[][] maps) {
-        // 이동할 4가지 방향 (상,하,좌,우)
-        int[] dx = {-1,1,0,0};
-        int[] dy = {0,0,-1,1};
-        
-        Queue<Point> queue = new LinkedList<>();
-        queue.add(new Point(0, 0)); // 처음 캐릭터 위치
-        
+        int rows = maps.length;
+        int cols = maps[0].length;
+
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 상하좌우
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, 0, 1}); // 시작 위치와 거리
+
         while (!queue.isEmpty()) {
-            Point currentPos = queue.poll();
-            int posX = currentPos.x;
-            int posY = currentPos.y;
-            
-            if (posX == n-1 && posY == m-1) break; // 상대팀 진영에 도착하면 탐색 종료
-            
-            // 현재 위치에서 4가지 방향으로 위치 확인
-            for (int i=0;i<4;i++) {
-                int checkX = posX + dx[i];
-                int checkY = posY + dy[i];
-                
-                // 배열의 범위를 벗어나는 경우 큐에 저장 안함
-                if (checkX < 0 || checkX >= n || checkY < 0 || checkY >= m) continue;
-                // 벽인 경우 큐에 저장 안함
-                if (maps[checkX][checkY] == 0) continue;
-                // 처음 방문한 곳에만 큐에 저장
-                if (maps[checkX][checkY] == 1) {
-                    maps[checkX][checkY] = maps[posX][posY] + 1;
-                    queue.add(new Point(checkX, checkY));
+            int[] current = queue.poll();
+            int row = current[0];
+            int col = current[1];
+            int distance = current[2];
+
+            if (row == rows - 1 && col == cols - 1) {
+                return distance; // 목적지에 도달한 경우 최단거리 반환
+            }
+
+            for (int[] dir : directions) {
+                int newRow = row + dir[0];
+                int newCol = col + dir[1];
+
+                if (newRow >= 0 
+                && newRow < rows 
+                && newCol >= 0 
+                && newCol < cols 
+                && maps[newRow][newCol] == 1) {
+                    maps[newRow][newCol] = 0; // 방문한 위치는 재방문하지 않도록 표시
+                    queue.offer(new int[]{newRow, newCol, distance + 1});
                 }
             }
         }
-        
-        // 다른 요소로 인해 아예 상대팀 진영에 도달하지 못할 경우 -1
-        return maps[n-1][m-1] == 1 ? -1 : maps[n-1][m-1];
-    }
-}
 
-class Point {
-    int x;
-    int y;
-    
-    Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+        return -1; // 목적지에 도달하지 못한 경우
     }
 }
