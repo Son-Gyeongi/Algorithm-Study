d44b0200fe539a2b90fdb4ba7e30b300ca2c7264
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2025-01-26 11:44:04 +0900

    [Silver I] Title: 미로 탐색, Time: 168 ms, Memory: 18840 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -20,14 +20,14 @@ public class Main {
         int[] dx = {-1,1,0,0};
         int[] dy = {0,0,-1,1};
         
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(0,0,1));
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0,0,1});
         
         while(!queue.isEmpty()) {
-            Point point = queue.poll();
-            int posX = point.x;
-            int posY = point.y;
-            int distance = point.distance;
+            int[] point = queue.poll();
+            int posX = point[0];
+            int posY = point[1];
+            int distance = point[2];
             
             if (posX == N-1 && posY == M-1) {
                 answer = distance;
@@ -41,24 +41,12 @@ public class Main {
                 if (newX >= 0 && newX < N && newY >= 0 && newY < M
                    && maze[newX][newY] == 1) {
                     // 행,열 범위를 벗어나지 않고 길이 막혀있지 않으면 queue에 넣기
-                    queue.offer(new Point(newX, newY, distance+1));
+                    queue.offer(new int[]{newX, newY, distance+1});
                     maze[newX][newY] = 0; // 지나온 길 탐색 하지 않음
                 }
             }
         }
         
         System.out.println(answer);
-    }
-}
-
-class Point {
-    int x;
-    int y;
-    int distance;
-    
-    Point(int x, int y, int distance) {
-        this.x = x;
-        this.y = y;
-        this.distance = distance;
     }
 }
