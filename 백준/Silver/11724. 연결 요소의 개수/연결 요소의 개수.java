51aae44957c4d708564309a7999a9948cd6ea76a
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2025-02-03 21:25:32 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1256 ms, Memory: 307776 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+    public static int[][] connect;
+    public static boolean[] visited;
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        if (m == 0) {
+            System.out.println(n);
+            return;
+        }
+        
+        connect = new int[n+1][n+1];
+        visited = new boolean[n+1];
+        for (int i=0;i<m;i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            
+            connect[u][v] = connect[v][u] = 1;
+        }
+        
+        int count = 0;
+        for (int i=1;i<visited.length;i++) {
+            if (visited[i] == false) {
+                dfs(i);
+                count++;
+            }
+        }
+        
+        System.out.println(count);
+    }
+    
+    public static void dfs(int start) {
+		    visited[start] = true;
+		           
+        for (int i=0;i<connect[0].length;i++) {
+            if (connect[start][i] == 1 && visited[i] == false) dfs(i);
+        }
+    }
+}
