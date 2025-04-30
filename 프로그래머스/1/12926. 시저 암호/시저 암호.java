22a07729234a47990b4a5098f8c06942acf75bc8
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2025-04-30 20:22:00 +0900

    [level 1] Title: 시저 암호, Time: 0.53 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,35 +1,16 @@
-import java.util.*;
-
 class Solution {
     public String solution(String s, int n) {
-        StringBuilder answer = new StringBuilder();
-        String[] sArr = s.split("");
-
-        String[] upperCase = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"
-                ,"Q","R","S","T","U","V","W","X","Y","Z"};
-        String[] lowerCase = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
-                ,"q","r","s","t","u","v","w","x","y","z"};
-
-        for(int i=0;i<sArr.length;i++) {
-            if(Objects.equals(sArr[i], " ")) {
-                answer.append(sArr[i]);
-                continue;
-            }
-            int idx;
-
-            for (int j = 0; j < upperCase.length; j++) {
-                if (Objects.equals(sArr[i], upperCase[j])) {
-                    idx = j+n;
-                    if(idx >= 26) idx -= 26;
-                    answer.append(upperCase[idx]);
-                } else if (Objects.equals(sArr[i], lowerCase[j])) {
-                    idx = j+n;
-                    if(idx >= 26) idx -= 26;
-                    answer.append(lowerCase[idx]);
-                }
-            }
+        char[] chArr = s.toCharArray();
+        
+        for (int i=0;i<chArr.length;i++) {
+            if (chArr[i] == ' ') continue;
+            int num = chArr[i] + n;
+            if (('a'<=chArr[i] && chArr[i]<='z') && num > 'z') num -= 26;
+            else if (('A'<=chArr[i] && chArr[i]<='Z') && num > 'Z') num -= 26;
+            
+            chArr[i] = (char) num;
         }
-
-        return answer.toString();
+        
+        return new String(chArr);
     }
 }
