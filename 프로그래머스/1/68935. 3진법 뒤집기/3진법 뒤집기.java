c241d268ec101b33074468e69679357a30eaabaa
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2025-05-02 16:58:48 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.11 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,12 +1,22 @@
 class Solution {
     public int solution(int n) {
-        StringBuilder sb = new StringBuilder();
-
-        while (n > 0) {
-            sb.append(n % 3);
+        int answer = 0;
+        StringBuilder builder = new StringBuilder();
+        
+        while (n >= 3) {
+            builder.append(n%3);
+            if (n/3 < 3) break;
             n /= 3;
         }
-
-        return Integer.parseInt(sb.toString(), 3);
+        if (n==1) builder.append(n%3);
+        if (n/3 != 0) builder.append(n/3);
+        
+        builder.reverse();
+        for (int i=0;i<builder.length();i++) {
+            int num = builder.charAt(i) - '0';
+            answer += num*Math.pow(3, i);
+        }
+        
+        return answer;
     }
 }
