16bf2a7d218a0d5c9b6368f40655efbfc58c9ed4
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2025-05-17 18:20:46 +0900

    [level 0] Title: A로 B 만들기, Time: 13.14 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -1,11 +1,25 @@
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.*;
 
 class Solution {
     public int solution(String before, String after) {
-        String beforeStr = Arrays.stream(before.split("")).sorted().collect(Collectors.joining());
-        String afterStr = Arrays.stream(after.split("")).sorted().collect(Collectors.joining());
+        Map<String, Integer> bMap = new HashMap<>();
+        Map<String, Integer> aMap = new HashMap<>();
+        
+        for (int i=0;i<before.length();i++) {
+            int value1 = bMap.getOrDefault(""+before.charAt(i),0);
+            bMap.put(""+before.charAt(i), value1+1);
+            
+            int value2 = aMap.getOrDefault(""+after.charAt(i),0);
+            aMap.put(""+after.charAt(i), value2+1);
+        }
+        
+        int count = 0;
+        for (String key : bMap.keySet()) {
+            if (bMap.get(key) == aMap.get(key)) count++;
+        }
+        
+        if (count == bMap.size()) return 1;
 
-        return beforeStr.equals(afterStr) ? 1 : 0;
+        return 0;
     }
 }
