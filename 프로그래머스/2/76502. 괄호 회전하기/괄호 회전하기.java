a5bf159cb8747d78e8e787cf01a6f1b1e57dd6d6
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2025-05-21 16:33:02 +0900

    [level 2] Title: 괄호 회전하기, Time: 67.22 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -3,53 +3,49 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
+        int count = 0;
         
-        String ss = s+s;
-        
-        for (int i=0;i<s.length();i++) {
-            Stack<Character> stack = new Stack<>();
-            boolean b = true;
-            int length = s.length();
+        do {
+            count++;
+            // 처음 닫는 괄호가 나오면 실패, 마지막에 여는 괄호가 나오면 실패
+            if (s.charAt(0) == ')' || s.charAt(0) == '}' || s.charAt(0) == ']'
+               || s.charAt(s.length()-1) == '(' || s.charAt(s.length()-1) == '{' || s.charAt(s.length()-1) == '[') {
+                String str = ""+s.charAt(0);
+                s = s.substring(1) + str;
+                continue;
+            }
             
-            for (int j=i;j<length+i;j++) {
-                
-                if (ss.charAt(j) == '[' || ss.charAt(j) == '{' || ss.charAt(j) == '(') {
-                    stack.push(ss.charAt(j));
-                } else if (stack.isEmpty()
-                          && (ss.charAt(j) == ']' || ss.charAt(j) == '}' || ss.charAt(j) == ')')) {
-                    b = false;
-                    break;
-                }
-                
-                if (!stack.isEmpty()) {
-                    if (ss.charAt(j) == ']') {
-                        if (stack.peek() == '[') stack.pop();
-                        else {
+            boolean b = true;
+            Stack<Character> stack = new Stack<>();
+            for (int i=0;i<s.length();i++) {
+                if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') stack.push(s.charAt(i));
+                else {
+                    if (stack.isEmpty()) {
+                        if (s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']') {
                             b = false;
-                            break;
                         }
+                        break;
                     }
-                    else if (ss.charAt(j) == '}') {
-                        if (stack.peek() == '{') stack.pop();
-                        else {
-                            b = false;
-                            break;
-                        }
-                    }
-                    else if (ss.charAt(j) == ')') {
-                        if (stack.peek() == '(') stack.pop();
-                        else {
-                            b = false;
-                            break;
-                        }
+                    else if ((stack.peek() == '(' && s.charAt(i) == ')')
+                            || (stack.peek() == '{' && s.charAt(i) == '}')
+                            || (stack.peek() == '[' && s.charAt(i) == ']')) {
+                        stack.pop();
                     }
                 }
             }
             
-            if (!stack.isEmpty()) continue;
+            if (!b) {
+                String str = ""+s.charAt(0);
+                s = s.substring(1) + str;
+                continue;
+            }
+            else if (stack.isEmpty()) {
+                answer++;
+            }
             
-            if (b) answer++;
-        }
+            String str = ""+s.charAt(0);
+            s = s.substring(1) + str;
+        } while (count < s.length());
         
         return answer;
     }
