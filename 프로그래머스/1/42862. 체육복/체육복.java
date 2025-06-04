10fce4e077cbef9699d73ddbed43d2f5eb1d6184
Son-Gyeongi <78200199+Son-Gyeongi@users.noreply.github.com>
2025-06-04 21:30:43 +0900

    [level 1] Title: 체육복, Time: 2.00 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,49 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
+        int[] students = new int[n];
+        Arrays.fill(students, 1); // 각 학생이 처음에 가지고 있던 체육복 1개
         
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-
-        // 1. 모든 n 학생 수를 Map에 넣기
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 1; i <= n; i++) {
-            map.put(i, 1);
+        // 여벌이 있는 학생
+        for (int i=0;i<reserve.length;i++) {
+            students[reserve[i]-1] += 1;
         }
-
-        // 2. lost 학생들은 0 값을 넣기
+        
+        // 잃어버린 학생
         for (int i=0;i<lost.length;i++) {
-            map.put(lost[i], 0);
+            students[lost[i]-1] -= 1;
         }
-
-        // 3. reserve를 확인해서 lost 학생들에게 옷을 빌려줄 수 있는지 확인
-        // 자기자신이 체육복 안 가져왔는지 확인
-        for (int i=0;i<reserve.length;i++) {
-            for (int j=0;j<lost.length;j++) {
-                if (reserve[i] == lost[j]) {
-                    map.put(lost[j], 1);
-                    reserve[i] = -1;
-                }
+        
+        for (int i=0;i<students.length-1;i++) {
+            if (students[i] == 2 && students[i+1] == 0) {
+                students[i] -= 1;
+                students[i+1] += 1;
+            } else if (students[i] == 0 && students[i+1] == 2) {
+                students[i] += 1;
+                students[i+1] -= 1;
             }
         }
-
-        // 다른 사람 체육복 빌리기
-        for (int i=0;i<reserve.length;i++) {
-            for (int j=0;j<lost.length;j++) {
-                if (reserve[i] == (lost[j] - 1) || reserve[i] == (lost[j] + 1)) {
-                    map.put(lost[j], 1);
-                    reserve[i] = -1; // 여벌 옷 빌려준 사람 제외
-                    lost[j] = -5; // 여벌 옷 빌린 사람 제외
-                }
-            }
-        }
-
-        // 4. Map을 확인해서 1인 학생의 수 구하기 (체육복이 있는 학생 수 세기)
-        for (int i=1;i<=map.size();i++) {
-            if (map.get(i) == 1) answer++;
-        }
-
+        
+        int answer = (int) Arrays.stream(students).filter(c -> c != 0).count();
         return answer;
     }
 }
